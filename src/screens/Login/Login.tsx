5af0d7e8aa6002bd1a3cd8b9e4b52dcdd34dede0
Manoel Bruno <manoelbrunop17@gmail.com>
2024-04-02 17:30:45 -0300

    "<feat:<chamada ao componente do botão de "Entrar" >"

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SafeAreaView, Text } from "react-native";
 import ButtonSocialGoogle from "@src/components/ButtonSocialGoogle/ButtonSocialGoogle";
 import ButtonSocial from "@src/components/ButtonSocial/ButtonSocial";
-
+import Button from "@src/components/Button/Button";
 import Input from "@src/components/Input";
 import {
     Container,
@@ -34,12 +34,14 @@ const Login: React.FC = () => {
                         iconSize={25}
                         iconName="mail-outline"
                         iconColor={theme.COLORS.TEXTDARK}
-                        placeholder="Digite seu e-mail"/>
+                        placeholder="Digite seu e-mail" />
                     <Input leftIcon
-                    iconSize={25}
-                    iconName="lock-closed-outline"
-                    iconColor={theme.COLORS.TEXTDARK}
-                    placeholder="Digite sua senha"/>
+                        iconSize={25}
+                        iconName="lock-closed-outline"
+                        iconColor={theme.COLORS.TEXTDARK}
+                        placeholder="Digite sua senha" />
+
+                        <Button />
                 </ContentBody>
 
                 <ContentFooter></ContentFooter>
