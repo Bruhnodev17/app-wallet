d3da261d6715893b426ae74f905feb9e93e8c3d2
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-13 18:47:35 -0300

    <feat>:<config inicial da navbar>

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,6 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import { Wallet } from '@src/screens/Tab/_Wallet'
+import { Notification } from '@src/screens/Tab/Notification'
+import { Report } from '@src/screens/Tab/Report'
+import { Settings } from '@src/screens/Tab/Settings'
+
 export const TabRoutes = () => {
     return (
         <View>
