87a542f173380c5b6aad9f23939d5d97242d176e
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-13 22:55:23 -0300

    <feat>:<card do cartão>

--- src/screens/Tab/_Wallet/Wallet.tsx
+++ src/screens/Tab/_Wallet/Wallet.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Container } from './styles'
+import { Container, Content, ViewContent,
+     ViewBalanceLeft,ViewBalanceRight, TitleValue, TitleCard,
+      TitleValueAcount, TitleCardBank } from './styles'
 
 import { Header } from '@src/components/Header/Header'
 
@@ -8,6 +10,22 @@ export const Wallet = () => {
   return (
     <Container>
         <Header/>
+
+        <ViewContent>
+          <Content>
+
+            <ViewBalanceLeft>
+                <TitleValue>Valor Total</TitleValue>
+                <TitleValueAcount>R$ 10.000</TitleValueAcount>
+            </ViewBalanceLeft>
+
+            <ViewBalanceRight>
+                <TitleCard>Cartão</TitleCard>
+                <TitleCardBank>Nubank</TitleCardBank>
+            </ViewBalanceRight>
+
+          </Content>
+        </ViewContent>
     </Container>
   )
 }
--- src/screens/Tab/_Wallet/styles.ts
+++ src/screens/Tab/_Wallet/styles.ts
@@ -1,5 +1,58 @@
 import styled from "styled-components/native";
+import theme from "@src/styles/theme";
 
 export const Container = styled.View`
+    align-items: center;
+    flex:1;
+`
+export const ViewContent = styled.View`
+    padding: 0 20px;
+    align-items: center;
+    justify-content: center;
+`
+
+
+export const Content = styled.View `
+    background-color: ${({})=> theme.COLORS.PURPLEDARK3};
+    width: 310px;
+    height: 146px;
+    border-radius: 50px;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+`
+
+export const ViewBalanceLeft = styled.View `
+
+    `
+
+export const ViewBalanceRight = styled.View`
 
 `
+
+export const TitleValue = styled.Text `
+    font-size: 16px;
+    color:${({})=> theme.COLORS.WHITE};
+    font-family: ${({}) => theme.FONTS.POPPINSREGULAR};
+    margin-bottom: 10px;
+    `
+export const TitleCard = styled.Text`
+    font-size: 16px;
+    color:${({})=> theme.COLORS.WHITE};
+    font-family: ${({}) => theme.FONTS.POPPINSREGULAR};
+    margin-bottom: 10px;
+`
+
+export const TitleValueAcount = styled.Text `
+    font-size: 20px;
+    color:${({})=> theme.COLORS.WHITE};
+    font-family: ${({}) => theme.FONTS.POPPINSBOLD};
+    `
+
+export const TitleCardBank = styled.Text `
+     font-size: 20px;
+    color:${({})=> theme.COLORS.WHITE};
+    font-family: ${({}) => theme.FONTS.POPPINSBOLD};
+`
+
+
