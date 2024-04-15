1fa6ca33093e29d6fe252a959d6d7697ba04b760
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-15 20:32:39 -0300

    <feat>:<Criação de screen profile>

--- src/screens/Tab/Settings/Settings.tsx
+++ src/screens/Tab/Settings/Settings.tsx
@@ -1,17 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { Container } from './styles'
+import { Container, ViewProfile, ProfileName, ViewRight, ViewIcon } from './styles'
+import theme from '@src/styles/theme'
 
 import { Header } from "../../../components/Header"
+import { CaretRight, User } from 'phosphor-react-native'
+import { useNavigation } from '@react-navigation/native'
 
 export const Settings = () => {
-  return (
-    <>
-    <Header appName='Configurações'/>
-    <Container>
+    const navigation = useNavigation()
 
-    </Container>
-    </>
-  )
+    const handleGoProfile = () => {
+       // navigation.navigate("")
+       console.warn("Clicou aqui")
+    }
+
+    return (
+        <>
+            <Header appName='Configurações' />
+            <Container>
+
+                <ViewProfile>
+                    <ViewIcon>
+                        <User
+                            size={27}
+                            weight='bold'
+                            color={theme.COLORS.PURPLEDARK2}
+                        />
+                    </ViewIcon>
+
+                    <ViewRight>
+                        <ProfileName>Manoel Bruno</ProfileName>
+                        <TouchableOpacity onPress={handleGoProfile}>
+                            <CaretRight
+                                size={25}
+                                weight='bold'
+                                color={theme.COLORS.GRAY3} />
+                        </TouchableOpacity>
+                    </ViewRight>
+
+                </ViewProfile>
+
+            </Container>
+        </>
+    )
 }
 
--- src/screens/Tab/Settings/styles.ts
+++ src/screens/Tab/Settings/styles.ts
@@ -1,7 +1,53 @@
+import theme from "@src/styles/theme";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
+    padding: 10px 15px;
+`
+
+export const ViewProfile = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding: 5px 15px;
+    margin-top: 15px;
+`
+
+export const ViewRight = styled.View`
+    flex-direction: row;
+    align-items: center;
+    flex: 1;
+    margin-left: 15px;
+    justify-content: space-between;
+`
+
+
+export const ProfileName = styled.Text`
+
+    justify-content: space-between;
+    font-size: 18px;
+    color: ${({ }) => theme.COLORS.BLACK};
+    font-family: ${({ }) => theme.FONTS.POPPINSMEDIUM};
+`
+
+export const ViewIcon = styled.View.attrs({
+    shadowColor: "#000000",
+    shadowOffset: {
+        width: 0,
+        height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 2.54,
+    elevation: 3
+})`
     align-items: center;
     justify-content: center;
+    width: 50px;
+    height: 50px;
+    background-color: ${({ }) => theme.COLORS.GRAY5};
+    border-radius: 50px;
+
+
+
 `
+
