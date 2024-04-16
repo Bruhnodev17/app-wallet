01d27cd5806cecc043215b477bb26ea6cbc2cbdb
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-16 13:30:31 -0300

    <feat>:<card screen>

--- src/screens/App/AddCard/AddCard.tsx
+++ src/screens/App/AddCard/AddCard.tsx
@@ -1,12 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Container } from './styles'
+import { Container, Title, Content, SubTitle } from './styles'
+
+import { GoBack } from '@src/components/GoBack'
 
 export const AddCard = () => {
   return (
+    <>
+    <GoBack/>
     <Container>
-      <Text>AddCard</Text>
+
+      <Title>Adicionar Cartão</Title>
+
+    <Content
+    source={require('../../../assets/addcard.png')}
+    >
+
+    </Content>
+
+    <SubTitle>
+        Adicione um novo cartão {'\n'} na carteira para uma vida mais fácil! 😄
+    </SubTitle>
+
     </Container>
+    </>
   )
 }
 
--- src/screens/App/AddCard/styles.ts
+++ src/screens/App/AddCard/styles.ts
@@ -1,6 +1,42 @@
 import styled from "styled-components/native";
+import theme from "@src/styles/theme";
 
 export const Container  = styled.View`
+    flex: 1;
+    width: 100%;
+    align-items: center;
+`
 
+export const Title = styled.Text`
+    width: 100%;
+    font-size: 30px;
+    color: ${({}) => theme.COLORS.PURPLEDARK3};
+    text-align: center;
+    margin-top: 50px;
+    font-family: ${({}) => theme.FONTS.POPPINSMEDIUM};
+`
+
+
+export const Content = styled.ImageBackground`
+
+    background-color: ${({}) => theme.COLORS.PURPLE};
+    width: 240px;
+    height: 340px;
+    margin-top: 40px;
+    align-items: center;
+    justify-content: center;
+    margin-top: 60px;
+    border-radius: 50px;
+
+`
+
+export const SubTitle = styled.Text`
+
+    text-align: center;
+    line-height: 20px;
+    font-size: 16px;
+    margin-top: 70px;
+    color: ${({}) => theme.COLORS.PURPLEDARK1};
+    font-family: ${({}) => theme.FONTS.POPPINSREGULAR};
 
 `
