08dfdcb31c8c40b0eb6744dc299d1fadd9fcc29f
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-16 11:49:45 -0300

    <feat>:<finalização de screen profile>

--- src/screens/App/Profile/Profile.tsx
+++ src/screens/App/Profile/Profile.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-import { Container, Header, Content, Footer, Avatar, ViewHeader, HeaderName, IconButton } from "./styles"
+import { Container, Header, Content, Footer, Avatar, ViewHeader, HeaderName, IconButton,
+    ViewFooter,ViewIconButton, TitleFooter } from "./styles"
 import { GoBack } from '@src/components/GoBack'
-import { Pen } from 'phosphor-react-native'
+import { Pen, Trash } from 'phosphor-react-native'
 import theme from '@src/styles/theme'
+
+import { Profile as ComponentProfile } from '@src/components/Profile'
 
 export const Profile = () => {
     return (
@@ -23,8 +26,40 @@ export const Profile = () => {
 
                     </ViewHeader>
                 </Header>
-                <Content></Content>
-                <Footer></Footer>
+                <Content>
+
+                    <ComponentProfile
+                        name='Conta Conectada'
+                        onPress={() => { }}
+                    />
+                    <ComponentProfile
+                        name='Privacidade e Segurança'
+                        onPress={() => { }}
+                    />
+                    <ComponentProfile
+                        name='Configuração de Login'
+                        onPress={() => { }}
+                    />
+                    <ComponentProfile
+                        name='Centro de Serviço'
+                        onPress={() => { }}
+                    />
+
+                </Content>
+                <Footer>
+                    <ViewFooter>
+                        <TouchableOpacity>
+                            <ViewIconButton>
+                                <Trash
+                                    size={32}
+                                    weight='regular'
+                                    color={theme.COLORS.PURPLE1} />
+                            </ViewIconButton>
+                        </TouchableOpacity>
+                        <TitleFooter>Deletar Conta</TitleFooter>
+                    </ViewFooter>
+
+                </Footer>
             </Container>
         </>
     )
--- src/screens/App/Profile/styles.ts
+++ src/screens/App/Profile/styles.ts
@@ -4,7 +4,7 @@ import styled from "styled-components/native"
 
 
 export const Container = styled.View`
-
+    padding: 5px;
 
 `
 
@@ -12,13 +12,11 @@ export const Header = styled.View`
     width: 100%;
     align-items: center;
     justify-content: center;
-
+    margin-bottom: 20px;
 
 `
 export const Content = styled.View`
-
-
-
+    margin-bottom: 70px;
 `
 export const Footer = styled.View`
 
@@ -38,12 +36,36 @@ export const ViewHeader = styled.View`
 `
 export const HeaderName = styled.Text`
     font-size: 22px;
-    color: ${({}) => theme.COLORS.GRAY2};
-    font-family: ${({}) => theme.FONTS.POPPINSLIGHT};
+    color: ${({}) => theme.COLORS.PURPLEDARK1};
+    font-family: ${({}) => theme.FONTS.POPPINSMEDIUM};
     margin-right: 15px;
+
 `
 
 export const IconButton = styled.TouchableOpacity`
 
 
+`
+
+export const ViewFooter = styled.View`
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`
+
+export const ViewIconButton = styled.View`
+    padding: 10px;
+    border-radius: 50px;
+    border-width: 1px;
+    border-color: ${({}) => theme.COLORS.GRAY5};
+    margin-bottom: 10px;
+
+`
+
+export const TitleFooter = styled.Text`
+    font-size: 20px;
+    color: ${({}) => theme.COLORS.PURPLE};
+    font-family: ${({})=> theme.FONTS.POPPINSREGULAR};
+    margin-bottom: 30px;
+    text-align: center;
 `
