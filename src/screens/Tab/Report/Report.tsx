23f24a3c888d96bda66b1fba9e4b812db09161a8
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-17 14:32:34 -0300

    <feat>:<finalização dos relatórios estáticos>

--- src/screens/Tab/Report/Report.tsx
+++ src/screens/Tab/Report/Report.tsx
@@ -1,26 +1,73 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Container, ImageBalance , AmountValue} from './styles'
+import {
+    Container, ImageBalance, AmountValue,
+    ViewContent, TitleAmount, TotalAmount,
+     FooterList, ContentFlat, ContentFlatHeader, Title, ButtonShowAll,
+    ButtonTitleShowAll, IconTransaction, DetailsTransaction, NameTransaction,
+    SubTitleTransaction, AmountTransaction,
+} from './styles'
 
 import { Header } from "../../../components/Header"
-
+import { FlatList, ScrollView, } from 'react-native'
+import { transaction } from '@src/utils/transctions'
 
 export const Report = () => {
-  return (
-    <>
-    <Header
-        iconLeft
-        typeReport
-        appName='Relatório'/>
-    <Container>
+    return (
+        <>
+            <Header
+                iconLeft
+                typeReport
+                appName='Relatório' />
+            <Container>
 
-      <ImageBalance resizeMode='contain'
-      source={require('../../../assets/estatistica.png')}
-      />
-      <AmountValue>R$ 500.00</AmountValue>
+                <ImageBalance resizeMode='contain'
+                    source={require('../../../assets/estatistica.png')}
+                />
+                <AmountValue>R$ 500.00</AmountValue>
 
-    </Container>
-    </>
-  )
+                <ViewContent>
+                    <TitleAmount>Valor Total</TitleAmount>
+                    <TotalAmount>R$ 10.000</TotalAmount>
+
+
+                </ViewContent>
+
+                <FooterList>
+                    <FlatList
+                        data={transaction}
+                        renderItem={({ item }) => (
+                            <ScrollView scrollEnabled={true} >
+                                <ContentFlat>
+
+                                    <IconTransaction
+                                        source={item.icon}
+                                    />
+                                    <DetailsTransaction>
+                                        <NameTransaction>{item.title}</NameTransaction>
+                                        <SubTitleTransaction>{item.subtitle}</SubTitleTransaction>
+                                    </DetailsTransaction>
+                                    <AmountTransaction>R$ {item.amount.toFixed(2)}</AmountTransaction>
+
+                                </ContentFlat>
+                            </ScrollView>
+                        )}
+
+                        ListHeaderComponent={
+                            <ContentFlatHeader>
+                                <Title>Minhas transações</Title>
+                                <ButtonShowAll>
+                                    <ButtonTitleShowAll>Mais Recentes</ButtonTitleShowAll>
+                                </ButtonShowAll>
+                            </ContentFlatHeader>
+                        }
+                        showsVerticalScrollIndicator={false}
+                    >
+
+                    </FlatList>
+                </FooterList>
+            </Container>
+        </>
+    )
 }
 
--- src/screens/Tab/Report/styles.ts
+++ src/screens/Tab/Report/styles.ts
@@ -22,3 +22,96 @@ export const AmountValue = styled.Text`
     top: 62px;
     left: 162px;
 `
+
+
+export const ViewContent = styled.View`
+    padding: 30px;
+    position: absolute;
+    top: 220px;
+`
+
+export const TitleAmount = styled.Text`
+    font-size: 22px;
+    text-align: center;
+    color: ${({}) => theme.COLORS.GRAY4};
+    font-family: ${({})=> theme.FONTS.POPPINSREGULAR};
+`
+
+export const TotalAmount = styled.Text`
+    text-align: center;
+    font-size: 33px;
+    font-family: ${({})=> theme.FONTS.POPPINSBOLD};
+    color: ${({}) => theme.COLORS.PURPLE};
+`
+
+
+export const FooterList = styled.View`
+    position: absolute;
+    width: 100%;
+    top: 320px;
+    margin-top: 20px;
+`
+
+
+export const ContentFlat = styled.View`
+    padding: 5px 20px;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const ContentFlatHeader = styled.View`
+    flex-direction: row;
+    padding: 20px;
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const Title = styled.Text`
+    font-size: 20px;
+    color: ${({}) => theme.COLORS.GRAY1};
+    font-family: ${({}) => theme.FONTS.POPPINSMEDIUM};
+    margin-left: 15px;
+`
+
+export const ButtonShowAll = styled.TouchableOpacity`
+    border-width: 2px;
+    border-radius: 30px;
+    border-color: ${({}) => theme.COLORS.PURPLEDARK1};
+    padding: 4px;
+`
+
+export const ButtonTitleShowAll = styled.Text`
+    font-size: 12px;
+    color: ${({}) => theme.COLORS.PURPLEDARK1};
+    font-family: ${({}) => theme.FONTS.POPPINSLIGHT};
+`
+
+
+export const IconTransaction = styled.Image`
+    width: 60px;
+    height: 60px;
+
+`
+
+export const DetailsTransaction = styled.View`
+    flex: 1;
+`
+
+export const NameTransaction = styled.Text`
+    font-size: 16px;
+    color: ${({}) => theme.COLORS.BLACK};
+    font-family: ${({}) => theme.FONTS.POPPINSBOLD};
+`
+
+export const SubTitleTransaction = styled.Text`
+    font-size:14px ;
+    color: ${({}) => theme.COLORS.GRAY3};
+    font-family: ${({}) => theme.FONTS.POPPINSREGULAR};
+
+`
+
+export const AmountTransaction = styled.Text`
+    font-size: 15px;
+    color: ${({}) => theme.COLORS.GRAY3};
+    font-family: ${({}) => theme.FONTS.POPPINSREGULAR};
+`
