68d062c8fc7fb5e88b4205b5aae8aacc8bc8c87a
Manoel Bruno <150696467+Bruhnodev17@users.noreply.github.com>
2024-04-18 13:37:22 -0300

    <feat>:<flatlist de novas notificações>

--- src/screens/Tab/Notification/Notification.tsx
+++ src/screens/Tab/Notification/Notification.tsx
@@ -1,29 +1,73 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
-import { Container, ContentTop, ContentTopTitle, ContentBody, ContentBodyTitle,  } from './styles'
+import { Container, ContentTop, ContentTopTitle, ContentBody, ContentBodyTitle, NewFlatList, RecentFlatList, ContentFlat, DataNotification, DataNotificationText, NewNotificationFlat,
+    TitleNotification, TitleNotificationText, DescriptionNotification, DescriptionNotificationText,
+    IconNotification
+} from './styles'
+
+import { new_notifications } from '@src/utils/new-notifications'
 
 import { Header } from "../../../components/Header"
+import { ArrowCircleUp } from 'phosphor-react-native'
+import theme from '@src/styles/theme'
 
 
 export const Notification = () => {
-  return (
-    <>
-        <Header
-         iconLeft
-         typeNotification
-         appName='Notificações'/>
-        <Container>
+    return (
+        <>
+            <Header
+                iconLeft
+                typeNotification
+                appName='Notificações' />
+            <Container>
 
-            <ContentTop>
-                <ContentTopTitle>Novo</ContentTopTitle>
-            </ContentTop>
+                <ContentTop>
+                    <ContentTopTitle>Novo</ContentTopTitle>
+                    <FlatList
+                        data={new_notifications}
+                        renderItem={({ item }) => (
+                            <ContentFlat>
+                                <NewNotificationFlat>
 
-            <ContentBody>
-                <ContentBodyTitle>Recentes</ContentBodyTitle>
-            </ContentBody>
+                                <DataNotification>
+                                    <DataNotificationText>
+                                        {item.datetime}
+                                    </DataNotificationText>
+                                </DataNotification>
 
-        </Container>
-    </>
-  )
+                                <TitleNotification>
+                                    <TitleNotificationText>
+                                        {item.title}
+                                    </TitleNotificationText>
+                                </TitleNotification>
+
+                                <DescriptionNotification>
+                                    <DescriptionNotificationText>
+                                        {item.type}
+                                    </DescriptionNotificationText>
+                                </DescriptionNotification>
+
+                                </NewNotificationFlat>
+
+                                <IconNotification>
+                                    <ArrowCircleUp size={32} weight='bold' color={theme.COLORS.GREEN1}/>
+                                </IconNotification>
+
+
+                            </ContentFlat>
+                        )}
+                        showsVerticalScrollIndicator={false}
+                    />
+
+                </ContentTop>
+
+                <ContentBody>
+                    <ContentBodyTitle>Recentes</ContentBodyTitle>
+                    <RecentFlatList></RecentFlatList>
+                </ContentBody>
+
+            </Container>
+        </>
+    )
 }
 
--- src/screens/Tab/Notification/styles.ts
+++ src/screens/Tab/Notification/styles.ts
@@ -4,26 +4,28 @@ import { FlatList } from "react-native";
 
 export const Container = styled.View`
     flex: 1;
+    align-items: center;
 `
 
 export const ContentTop = styled.View`
     width: 100%;
-    height: 40%;
+    height: 50%;
     align-items: center;
-    padding: 30px 10px;
+    padding: 20px 10px;
 `
 
 export const ContentTopTitle = styled.Text`
-    font-size: 24px;
+    font-size: 20px;
     font-family:${({}) => theme.FONTS.POPPINSBOLD};
     color: ${({}) => theme.COLORS.PURPLE};
+
 `
 
 export const ContentBody = styled.View`
     flex: 1;
     width: 100%;
     align-items: center;
-    padding: 30px 10px;
+    padding: 20px 10px;
 `
 
 export const ContentBodyTitle = styled.Text`
@@ -32,13 +34,77 @@ export const ContentBodyTitle = styled.Text`
     color: ${({}) => theme.COLORS.PURPLE};
 `
 
-export const NewFlatList = styled(FlatList)`
+export const ContentFlat = styled.View`
 
+    margin-top: 10px;
+    border-radius: 17px;
+    background-color: white;
+    align-items: center;
+`
+export const NewNotificationFlat = styled.View`
+    padding: 10px;
+    width: 90%;
+    height: 100px;
+`
+
+export const NewFlatList = styled(FlatList).attrs({
+    showsVerticalScrollIndicator: false,
+
+})
+`
+`
+
+export const IconNotification = styled.TouchableOpacity`
+    width: 50px;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 0;
+    top: 5;
+`
+
+export const DataNotification = styled.View`
 
 `
 
-export const RecentFlatList = styled(FlatList)`
+export const DataNotificationText = styled.Text`
+    font-size: 12px;
+    color: ${({}) => theme.COLORS.GRAY4};
+    margin-bottom: 5px;
+`
 
+export const TitleNotification = styled.View`
 
 `
+
+export const TitleNotificationText = styled.Text`
+    font-size: 14px;
+    font-weight: 500;
+`
+
+export const DescriptionNotification = styled.View`
+
+`
+
+export const DescriptionNotificationText = styled.Text`
+    font-size: 14px;
+    color: ${({}) => theme.COLORS.PURPLE};
+    margin-top: 10px;
+`
+
+
+
+
+//
+
+
+export const RecentFlatList = styled(FlatList).attrs({
+    showsVerticalScrollIndicator: false,
+
+})
+`
+
+`
+
+
 
